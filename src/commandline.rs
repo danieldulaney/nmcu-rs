b6890ee1b3b2fa25db7b2c981309c146c7ccd7a2
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-04-13 03:15:03 +0000

    Fixed compiling issue

--- src/commandline.rs
+++ src/commandline.rs
@@ -29,32 +29,34 @@ impl Recieve for CommandLine {
     }
 
     fn recieve_serial(&mut self, payload: Vec<u8>) -> Result<Response, ()> {
-        let line = self.last_line.as_bytes();
-
         let mut line_index = 0;
         let mut payload_index = 0;
 
-        loop {
-            if let (Some(&line_cur), Some(&payload_cur)) =
-                (line.get(line_index), payload.get(payload_index))
-            {
-                if line_cur == payload_cur {
-                    // They match, consume one of each
-                    line_index += 1;
-                    payload_index += 1;
-                } else if line_cur == '\n' as u8 && payload_cur == '\r' as u8
+        {
+            let line = self.last_line.as_bytes();
+
+            loop {
+                if let (Some(&line_cur), Some(&payload_cur)) =
+                    (line.get(line_index), payload.get(payload_index))
+                {
+                    if line_cur == payload_cur {
+                        // They match, consume one of each
+                        line_index += 1;
+                        payload_index += 1;
+                    } else if line_cur == '\n' as u8 && payload_cur == '\r' as u8
                     // The line looks like "...\n", while the payload looks like "...\r\n"
                     // Consume one of the line and two of the payload
                     && line.get(line_index + 1) == None
-                    && payload.get(payload_index + 1) == Some(&('\n' as u8))
-                {
-                    line_index += 1;
-                    payload_index += 2;
+                        && payload.get(payload_index + 1) == Some(&('\n' as u8))
+                    {
+                        line_index += 1;
+                        payload_index += 2;
+                    } else {
+                        break;
+                    }
                 } else {
                     break;
                 }
-            } else {
-                break;
             }
         }
 
