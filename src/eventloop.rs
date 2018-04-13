264e926db1330a66fbf82fe395c3b411903da453
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-04-13 02:24:28 +0000

    Factored out event loop into new file

---
+++ src/eventloop.rs
@@ -0,0 +1,113 @@
+use std::io;
+use std::os::unix::io::AsRawFd;
+use std::io::{Read, Write};
+
+use mio::{Events, Poll, PollOpt, Ready, Token, unix::EventedFd};
+use mio_serial::{Serial, SerialPortSettings};
+
+#[derive(Debug)]
+pub struct Response {
+    stdout: Option<Box<[u8]>>,
+    serial: Option<Box<[u8]>>,
+    terminate: bool,
+}
+
+impl Response {
+    pub fn none() -> Response {
+        Response {
+            stdout: None,
+            serial: None,
+            terminate: false,
+        }
+    }
+
+    pub fn to_stdout(b: Box<[u8]>) -> Response {
+        Response {
+            stdout: Some(b),
+            serial: None,
+            terminate: false,
+        }
+    }
+
+    pub fn to_serial(b: Box<[u8]>) -> Response {
+        Response {
+            stdout: None,
+            serial: Some(b),
+            terminate: false,
+        }
+    }
+}
+
+pub trait Recieve {
+    fn startup(&mut self) -> Response;
+    fn recieve_stdin(&mut self, line: String) -> Result<Response, ()>;
+    fn recieve_serial(&mut self, payload: Vec<u8>) -> Result<Response, ()>;
+    fn shutdown(&mut self) -> Response;
+}
+
+const STDIN_TOKEN: Token = Token(0);
+const SERIAL_TOKEN: Token = Token(1);
+
+pub fn run(mut reciever: Box<Recieve>, serial_settings: &SerialPortSettings) -> usize {
+    let poll = Poll::new().unwrap();
+
+    let stdin = io::stdin();
+
+    poll.register(
+        &EventedFd(&stdin.as_raw_fd()),
+        STDIN_TOKEN,
+        Ready::readable(),
+        PollOpt::edge(),
+    ).unwrap();
+
+    let mut serial = Serial::from_path("/dev/ttyS0", &serial_settings).unwrap();
+
+    poll.register(&serial, SERIAL_TOKEN, Ready::readable(), PollOpt::edge())
+        .unwrap();
+
+    let mut events = Events::with_capacity(1024);
+
+    reciever.startup();
+
+    loop {
+        poll.poll(&mut events, None).unwrap();
+
+        for event in events.iter() {
+            let response = match (event.token(), event.readiness()) {
+                (STDIN_TOKEN, r) if r.is_readable() => {
+                    let mut line = String::new();
+                    stdin.read_line(&mut line).unwrap();
+                    reciever.recieve_stdin(line).unwrap()
+                }
+                (SERIAL_TOKEN, r) if r.is_readable() => {
+                    let mut buffer = Vec::new();
+                    serial.read_to_end(&mut buffer);
+                    reciever.recieve_serial(buffer).unwrap()
+                }
+                _ => {
+                    eprintln!("Unrecognized event {:?}", event);
+                    Response::none()
+                }
+            };
+
+            // IO flushing here is actually super important
+            if let Some(b) = response.stdout {
+                io::stdout().write(&b).unwrap();
+                io::stdout().flush().unwrap();
+            }
+
+            if let Some(b) = response.serial {
+                serial.write(&b).unwrap();
+                serial.flush().unwrap();
+            }
+
+            if response.terminate {
+                break;
+            }
+        }
+    }
+
+    reciever.shutdown();
+
+    return 0;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
-extern crate nix;
 extern crate mio;
 extern crate mio_serial;
 
-use std::io;
-use std::os::unix::io::AsRawFd;
+mod eventloop;
+
+pub use eventloop::{Recieve, Response};
+
 use std::time::Duration;
-use std::io::{Read, Write};
-use std::ascii::escape_default;
-use std::ops::Deref;
 
-use nix::sys::termios::{tcgetattr, tcsetattr, LocalFlags, Termios, SetArg};
-
-use mio::{Events, Poll, PollOpt, Ready, Token, unix::EventedFd};
-use mio_serial::{BaudRate, DataBits, FlowControl, Parity, SerialPortSettings, StopBits,
-                 unix::Serial};
+use mio_serial::{BaudRate, DataBits, FlowControl, Parity, SerialPortSettings, StopBits};
 
 const SETTINGS: SerialPortSettings = SerialPortSettings {
     baud_rate: BaudRate::Baud115200,
@@ -24,55 +18,13 @@ const SETTINGS: SerialPortSettings = SerialPortSettings {
     timeout: Duration::from_millis(1),
 };
 
-#[derive(Debug)]
-struct Response {
-    stdout: Option<Box<[u8]>>,
-    serial: Option<Box<[u8]>>,
-    terminate: bool,
-}
-
-impl Response {
-    fn none() -> Response {
-        Response {
-            stdout: None,
-            serial: None,
-            terminate: false,
-        }
-    }
-
-    fn to_stdout(b: Box<[u8]>) -> Response {
-        Response {
-            stdout: Some(b),
-            serial: None,
-            terminate: false,
-        }
-    }
-
-    fn to_serial(b: Box<[u8]>) -> Response {
-        Response {
-            stdout: None,
-            serial: Some(b),
-            terminate: false,
-        }
-    }
-}
-
-trait Recieve {
-    fn startup(&mut self) -> Response;
-    fn recieve_stdin(&mut self, line: &str) -> Result<Response, ()>;
-    fn recieve_serial(&mut self, payload: &[u8]) -> Result<Response, ()>;
-    fn shutdown(&mut self) -> Response;
-}
-
 struct CommandLine {
-    original_config: Option<Termios>,
     last_line: String,
 }
 
 impl CommandLine {
     fn new() -> Box<CommandLine> {
         Box::new(CommandLine {
-            original_config: None,
             last_line: String::new(),
         })
     }
@@ -80,19 +32,10 @@ impl CommandLine {
 
 impl Recieve for CommandLine {
     fn startup(&mut self) -> Response {
-        /*
-        let mut config = tcgetattr(io::stdin().as_raw_fd()).unwrap();
-        self.original_config = Some(config.clone());
-
-        config.local_flags.remove(LocalFlags::ECHO);
-
-        tcsetattr(io::stdin().as_raw_fd(), SetArg::TCSAFLUSH, &config).unwrap();
-        */
-
         Response::none()
     }
 
-    fn recieve_stdin(&mut self, line: &str) -> Result<Response, ()> {
+    fn recieve_stdin(&mut self, line: String) -> Result<Response, ()> {
         self.last_line = line.to_owned();
 
         Ok(Response::to_serial(
@@ -100,103 +43,30 @@ impl Recieve for CommandLine {
         ))
     }
 
-    fn recieve_serial(&mut self, payload: &[u8]) -> Result<Response, ()> {
-
+    fn recieve_serial(&mut self, payload: Vec<u8>) -> Result<Response, ()> {
         let mut print_from = 0;
-        
+
         for (i, (line, &payload)) in self.last_line.bytes().zip(payload.iter()).enumerate() {
             if line == payload {
                 print_from = i + 1;
             }
         }
 
-        //eprintln!("Last_line was {:?}; payload is {:?}, skipping first {}", self.last_line, payload, print_from);
-
-        Ok(Response::to_stdout(payload.iter().skip(print_from).map(|x| *x).collect::<Vec<u8>>().into_boxed_slice()))
+        Ok(Response::to_stdout(
+            payload
+                .iter()
+                .skip(print_from)
+                .map(|x| *x)
+                .collect::<Vec<u8>>()
+                .into_boxed_slice(),
+        ))
     }
 
     fn shutdown(&mut self) -> Response {
-        /*
-        match self.original_config {
-            Some(ref c) => tcsetattr(io::stdin().as_raw_fd(), SetArg::TCSAFLUSH, c).unwrap(),
-            None => {},
-        }
-        */
-
         Response::none()
     }
 }
 
 fn main() {
-    event_loop(CommandLine::new(), &SETTINGS);
-}
-
-const STDIN_TOKEN: Token = Token(0);
-const SERIAL_TOKEN: Token = Token(1);
-
-fn event_loop(mut reciever: Box<Recieve>, serial_settings: &SerialPortSettings) {
-    let poll = Poll::new().unwrap();
-
-    let stdin = io::stdin();
-
-    poll.register(
-        &EventedFd(&stdin.as_raw_fd()),
-        STDIN_TOKEN,
-        Ready::readable(),
-        PollOpt::edge(),
-    ).unwrap();
-
-    let mut serial = Serial::from_path("/dev/ttyS0", &serial_settings).unwrap();
-
-    poll.register(&serial, SERIAL_TOKEN, Ready::readable(), PollOpt::edge())
-        .unwrap();
-
-    let mut events = Events::with_capacity(1024);
-
-    reciever.startup();
-
-    loop {
-        poll.poll(&mut events, None).unwrap();
-
-        //eprintln!("Top of event loop");
-
-        for event in events.iter() {
-            //eprintln!("Processing {:?}", event);
-
-            let response = match (event.token(), event.readiness()) {
-                (STDIN_TOKEN, r) if r.is_readable() => {
-                    let mut line = String::new();
-                    stdin.read_line(&mut line).unwrap();
-                    reciever.recieve_stdin(&line).unwrap()
-                }
-                (SERIAL_TOKEN, r) if r.is_readable() => {
-                    //let mut buffer = [0; 1];
-                    //serial.read(&mut buffer);
-                    let mut buffer = Vec::new();
-                    serial.read_to_end(&mut buffer);
-                    reciever.recieve_serial(&buffer).unwrap()
-                }
-                _ => {
-                    println!("Unrecognized event {:?}", event);
-                    Response::none()
-                }
-            };
-
-            if let Some(b) = response.stdout {
-                io::stdout().write(&b).unwrap();
-                io::stdout().flush().unwrap();
-            }
-
-            if let Some(b) = response.serial {
-                serial.write(&b).unwrap();
-                serial.flush().unwrap();
-            }
-
-            if response.terminate {
-                break;
-            }
-        }
-    }
-
-    reciever.shutdown();
+    eventloop::run(CommandLine::new(), &SETTINGS);
 }
